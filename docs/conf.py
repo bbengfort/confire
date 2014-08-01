ea40eb677575116dc093c69ff1706bb5ee70c767
Benjamin Bengfort <benjamin@bengfort.com>
2014-08-01 11:20:08 -0400

    revert for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -81,7 +81,8 @@ exclude_patterns = ['_build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'tango'
+#pygments_style = 'tango'
+pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
@@ -94,7 +95,8 @@ pygments_style = 'tango'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+# html_theme = 'nature'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
